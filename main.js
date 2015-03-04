38f1824bca602cb005fd9fcc4f86f348d31f0319
Matas Kairaitis <mataskairaitis@Matass-MacBook-Air.local>
2015-03-04 15:26:59 +0100

    Fixed some misbehaving floats

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 var currency = require('y-currency');
 
-var amount = parseInt(process.argv[2]);
+var amount = parseFloat(process.argv[2]);
 var from = process.argv[3].toString();
 var to = process.argv[4].toString();
 
