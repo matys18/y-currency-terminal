20a7df439ff920984a43c9eab5ba9f7aff49f8e5
Matas Kairaitis <matas.kairaitis@gmail.com>
2016-01-04 02:16:25 +0100

    Update to version 1.0.6

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 #!/usr/bin/env node
-
 var currency = require('y-currency');
 
 var amount = parseFloat(process.argv[2]);
 var from = process.argv[3].toString();
 var to = process.argv[4].toString();
 
-currency.convert(amount,from,to,function(err,converted){
+currency.convert(amount, from, to, function(err,converted) {
 
-	if (err){
-        console.log(err);
-    }
+	if (err) console.error(err);
 
-	console.log('\n' + amount+from+' = '+converted+to);
+	console.log('\n', amount, from, ' = ', converted, to);
 });
